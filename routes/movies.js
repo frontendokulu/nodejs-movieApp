ff8ab2d48a70f1354dfaf4aa1c4e4782459a9456
frontendokulu <frontendokulu@gmail.com>
2020-04-24 13:46:59 +0300

    promise yapısı, list all movies, updateById, deleteById işlemleri eklendi

--- routes/movies.js
+++ routes/movies.js
@@ -3,46 +3,74 @@ const router = express.Router();
 
 //model dosyasını al
 const Movie = require('../models/Movie');
-
-/* GET movies listing. */
-router.post('/', (req, res, next) => {
-  //body parser, yapılan isteği göndermek için kullanılır.
-  //bu şekilde post edilen data ya erişilebilir.
-  // const data = req.body;
-  //gönderilen verinin çıktısı json şeklinde olduğu için object ile veriye erişilebilir.
-  // const data = req.body.year;
-  // res.json(data.title);
-//  es6 daki desctructing yapısı ile veriyi aşağıdaki gibi alabiliriz. Kodu kısaltır.
-//   const {title, imdb_score, category, year, country} = req.body;
-  //gelen data modelden oluşturduğumuz nesnenin içine ekledik.
-  /*
-  const movie = new Movie({
-    title : title,
-    imdb_score :imdb_score,
-    category : category,
-    year : year,
-    country :country
+/* List All Movies */
+router.get('/',(req,res) =>{
+  //tüm fimleri al
+  const promise = Movie.find({ });
+  promise.then((data) =>{
+    res.json(data);
+  }).catch((err) =>{
+    res.json(err);
   });
-  */
-  //Yukarıda tüm elemanları tek tek atıyoruz,
-  //Bu yöntem de ise geleni otomatik parçalar ve isimlerine göre elemanlara atar.
-  //kral hareket
+});
+
+/* List ById Movies */
+router.get('/:movie_id',(req, res, next) =>{
+  const promise = Movie.findById((req.params.movie_id));
+
+  promise.then((movie) =>{
+    if (!movie) {
+      next({message: 'The movie was not found', code: 99});
+    }
+    res.json(movie);
+  }).catch((err) =>{
+    res.json(err);
+  });
+});
+
+
+/* Save Movie */
+router.post('/', (req, res, next) => {
   const movie = new Movie(req.body);
-/*
-    movie.save((err,data) =>{
-      if (err){res.send(err);}
-      else{res.send(data);}
-    });
-  */
-  //Yukardaki işlemin then catch promise yapısı ile kullanımı
-  //aralarında hiç bir fark yok
   const promise = movie.save();
   promise.then((data) => {
-    res.json({status : 1});
+    res.json(data);
   }).catch((err) =>{
     res.json(err);
   });
 
+});
+
+/* Update ById Movies */
+router.put('/:movie_id',(req, res, next) =>{
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    { new :true }
+  );
+
+  promise.then((movie) =>{
+    if (!movie) {
+      next({message: 'The movie was not found', code: 99});
+    }
+    res.json(movie);
+  }).catch((err) =>{
+    res.json(err);
+  });
+});
+
+/* Delete ById Movie */
+router.delete('/:movie_id',(req, res, next) =>{
+  const promise = Movie.findByIdAndRemove((req.params.movie_id));
+
+  promise.then((movie) =>{
+    if (!movie) {
+      next({message: 'The movie was not found', code: 99});
+    }
+    res.json({status : 'Film Silindi'});
+  }).catch((err) =>{
+    res.json(err);
+  });
 });
 
 module.exports = router;
