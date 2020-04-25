ab6bebdf436c59dae06ea5dd952db28d44518c47
frontendokulu <frontendokulu@gmail.com>
2020-04-25 14:49:26 +0300

    add director info to movies, movies validation, directors validation

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,33 @@ const MovieSchema = new Schema({
     director_id : Schema.Types.ObjectId,
     title : {
         type :String,
-        required :true
+        required :[true, '`{PATH} alanı zorunludur.'],
+        maxlength : [30, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden az olmak zorundadır.'],
+        minlength : [1, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden fazla olmak zorundadır.']
     },
-    category : String,
-    country : String,
-    year : Number,
-    imdb_score : Number,
+    category : {
+        type: String,
+        required :[true, '`{PATH} alanı zorunludur.'],
+        maxlength : [80, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden az olmak zorundadır.'],
+        minlength : [4, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden fazla olmak zorundadır.']
+    },
+    country : {
+        type : String,
+        required :[true, '`{PATH} alanı zorunludur.'],
+        maxlength : [50, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden az olmak zorundadır.'],
+        minlength : [3, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden fazla olmak zorundadır.']
+    },
+    year : {
+        type : Number,
+        maxlength : [2020, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden az olmak zorundadır.'],
+        minlength : [1900, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden fazla olmak zorundadır.']
+    },
+
+    imdb_score : {
+        type : Number,
+        maxlength : [10, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden az olmak zorundadır.'],
+        minlength : [0, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden fazla olmak zorundadır.']
+    },
     createdAt : {
         type: Date,
         default : Date.now
--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,19 @@ const Movie = require('../models/Movie');
 /* List All Movies */
 router.get('/',(req,res) =>{
   //tüm fimleri al
-  const promise = Movie.find({ });
+  const promise = Movie.aggregate([
+    {
+      $lookup : {
+        from : 'directors',
+        localField : 'director_id',
+        foreignField : '_id',
+        as : 'director'
+      }
+    },
+    {
+      $unwind : '$director'
+    }
+  ]);
   promise.then((data) =>{
     res.json(data);
   }).catch((err) =>{
